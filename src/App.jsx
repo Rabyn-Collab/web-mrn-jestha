a2691e1fe38c968a38bc306a9614a60bf71862d5
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-17 08:01:19 +0545

    sabai add gare

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import HomePage from "./pages/home/HomePage";
+import { createBrowserRouter } from "react-router"
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import { RouterProvider } from "react-router-dom";
+import Contact from "./pages/contact/Contact";
+import NotFound from "./pages/not-found/NotFound";
 
 export default function App() {
 
+  const router = createBrowserRouter([
 
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: <About />
+    },
+    {
+      path: 'contact',
+      element: <Contact />
+    },
 
-  return (
-    <div>
+    {
+      path: '*',
+      element: <NotFound />
+    }
+  ]);
 
-      <HomePage />
-
-
-
-
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1 className="text-3xl font-bold">This is Home Page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium ut, nobis, reprehenderit est atque consectetur vero et sit ipsa aspernatur maiores voluptates quae saepe alias soluta sequi ipsum dolorem quod!</p>
+
+    </div>
+  )
+}
