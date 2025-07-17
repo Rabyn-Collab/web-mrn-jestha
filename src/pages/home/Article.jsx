f072c0435316901ee61515921df6a7c6b423514d
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-17 07:41:09 +0545

    sabai add gare

--- src/pages/home/Article.jsx
+++ src/pages/home/Article.jsx
@@ -8,6 +8,8 @@ export default function Article({ sec }) {
         <HiCubeTransparent size={210} color="red" />
       </div>}
 
+      {/* {sec ? <img src="https://plus.unsplash.com/premium_photo-1752367289570-090e5807428d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8" alt="" /> : <img src="https://images.unsplash.com/photo-1682686578289-cf9c8c472c9b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />} */}
+
       <div className="space-y-10">
         <h1 className="text-3xl">Lorem Ipsum</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, rem porro. Doloremque consectetur aut perspiciatis similique, fuga eius ut expedita error quidem ab, id provident neque. Asperiores amet adipisci sint.</p>
