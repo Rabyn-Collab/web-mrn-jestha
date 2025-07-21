91744540b86bc0406d6c1808e2c6c9f5645705c1
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-21 07:21:03 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="bg-black text-white p-3 flex items-baseline justify-between">
 
-      <h1 className="text-2xl">React Router</h1>
+      <h1 className="text-2xl">React Hooks</h1>
 
       <nav className="space-x-7">
         <NavLink className={({ isActive }) => isActive ? 'bg-white text-black' : 'text-white'} to={'/about'}>About</NavLink>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,47 @@
-import { Button } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
+import { Formik } from "formik";
 import { useState } from "react"
 
-
 export default function Home() {
-
   const [data, setData] = useState([]);
-
-
-  const handleData = () => {
-    setData((prev) => [...prev, 'hello']);
-  }
-
-  console.log(data);
 
   return (
     <div className="p-5">
 
-      <Button onClick={handleData}>Click To Show Data</Button>
+      <Formik
+        initialValues={{
+          title: ''
+        }}
+        onSubmit={(val, { resetForm }) => {
+          console.log(val);
+          resetForm();
+        }}
+      >
+        {
+          ({ handleChange, handleSubmit, values }) => (
+            <form
+              onSubmit={handleSubmit}
+              className="max-w-[300px] space-y-4">
+              <div>
+                <Input
+                  onChange={handleChange}
+                  value={values.title}
+                  name="title"
+                  label="Title" />
+              </div>
+              <Button type="submit">Submit</Button>
+
+            </form>
+          )
+        }
+
+
+
+      </Formik>
+
+
+
+
 
     </div>
   )
