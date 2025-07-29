7943604a6674bdc5263478a4396e9bbef752058e
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-29 07:24:54 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,46 +1,22 @@
-import { Button, Input } from "@material-tailwind/react";
-import { Formik } from "formik";
-import { useState } from "react"
+import { Button } from "@material-tailwind/react";
+import { useRef } from "react";
+
 
 export default function Home() {
-  const [data, setData] = useState([]);
+
+  const secRef = useRef();
+
+  const scrollToSec = () => {
+    secRef.current.scrollIntoView({
+      behavior: "smooth"
+    })
+  }
+
+
 
   return (
-    <div className="p-5">
-
-      <Formik
-        initialValues={{
-          title: ''
-        }}
-        onSubmit={(val, { resetForm }) => {
-          console.log(val);
-          resetForm();
-        }}
-      >
-        {
-          ({ handleChange, handleSubmit, values }) => (
-            <form
-              onSubmit={handleSubmit}
-              className="max-w-[300px] space-y-4">
-              <div>
-                <Input
-                  onChange={handleChange}
-                  value={values.title}
-                  name="title"
-                  label="Title" />
-              </div>
-              <Button type="submit">Submit</Button>
-
-            </form>
-          )
-        }
-
-
-
-      </Formik>
-
-
-
+    <div className="p-5 space-y-5">
+      <Button onClick={scrollToSec}>Section 2</Button>
 
 
     </div>
