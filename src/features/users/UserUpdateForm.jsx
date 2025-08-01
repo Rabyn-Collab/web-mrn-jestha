f9f56be931efd393bf4d55d6fb082df6134d3517
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-01 07:12:25 +0545

    changes

--- src/features/users/UserUpdateForm.jsx
+++ src/features/users/UserUpdateForm.jsx
@@ -3,11 +3,14 @@ import { Formik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import { valSchema } from "./UserAddForm.jsx";
+import { updateUser } from "./userSlice.js";
 
 
 export default function UserUpdateForm() {
   const { id } = useParams();
   const { users } = useSelector((state) => state.userSlice);
+
+  const user = users.find((user) => user.id === id);
 
   const dispatch = useDispatch();
   const nav = useNavigate();
@@ -16,16 +19,16 @@ export default function UserUpdateForm() {
 
       <Formik
         initialValues={{
-          username: '',
-          email: '',
-          habits: [],
-          gender: '',
-          country: '',
-          bio: ''
+          username: user.username,
+          email: user.email,
+          habits: user.habits,
+          gender: user.gender,
+          country: user.country,
+          bio: user.bio
         }}
         onSubmit={(val) => {
-          // dispatch(addUser({ ...val, id: nanoid() }));
-          // nav(-1);
+          dispatch(updateUser({ ...val, id: user.id }));
+          nav(-1);
 
         }}
         validationSchema={valSchema}
@@ -37,12 +40,14 @@ export default function UserUpdateForm() {
             <div>
               <Input
                 onChange={handleChange}
+                value={values.username}
                 label="Username" name="username" />
               {errors.username && touched.username && <h1 className="text-pink-700">{errors.username}</h1>}
             </div>
             <div>
               <Input
                 onChange={handleChange}
+                value={values.email}
                 label="Email" name="email" type="email" />
               {errors.email && touched.email && <h1 className="text-pink-700">{errors.email}</h1>}
             </div>
@@ -50,26 +55,31 @@ export default function UserUpdateForm() {
             <div>
               <Typography>Select Your Habits</Typography>
               <Checkbox
+                checked={values.habits.includes('dance')}
                 onChange={handleChange}
                 label='Dance' value={'dance'} name="habits" />
               <Checkbox
                 onChange={handleChange}
+                checked={values.habits.includes('sing')}
                 label='Sing' value={'sing'} name="habits" />
               {errors.habits && touched.habits && <h1 className="text-pink-700">{errors.habits}</h1>}
             </div>
             <div>
               <Typography>Select Your Genders</Typography>
               <Radio
+                checked={values.gender === 'male'}
                 onChange={handleChange}
                 label='Male' value={'male'} name="gender" />
               <Radio
                 onChange={handleChange}
+                checked={values.gender === 'female'}
                 label='Female' value={'female'} name="gender" />
               {errors.gender && touched.gender && <h1 className="text-pink-700">{errors.gender}</h1>}
             </div>
             <div className="space-y-2">
               <Typography>Select Your Country</Typography>
               <Select
+                value={values.country}
                 onChange={(e) => setFieldValue('country', e)}
                 label="Select Country" name="country">
                 <Option value="Nepal">Nepal</Option>
@@ -81,7 +91,9 @@ export default function UserUpdateForm() {
             </div>
 
             <div>
-              <Textarea label="Bio" onChange={handleChange} name="bio"></Textarea>
+              <Textarea
+                value={values.bio}
+                label="Bio" onChange={handleChange} name="bio"></Textarea>
               {errors.bio && touched.bio && <h1 className="text-pink-700">{errors.bio}</h1>}
             </div>
 
