4471e87c666fe1f1e418c4864a5a36d504cd8a83
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-12 07:44:05 +0545

    changes

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+import mongoose from 'mongoose';
+
+//mvc
+
+
+const app = express();
+const port = 5000;
+
+mongoose.connect('mongodb+srv://psg017597:pass900@cluster0.yqujtfd.mongodb.net/Shop').then(() => {
+  app.listen(port, () => {
+    console.log('database connect and server is running');
+  });
+}).catch((err) => {
+  console.log(err);
+})
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'hello' });
+});
+
+app.use(productRoutes);
+
+
+
+
+
+
+
+
+
+
---
+++ routes/productRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { createProduct, getProduct, getProducts, removeProduct, updateProduct } from '../controllers/productController.js';
+
+
+
+
+const router = express.Router();
+
+//getAllProducts,getTopRatedProducts, searchProduct,productAdd, 
+router.route('/products').get(getProducts).post(createProduct);
+
+
+//   getProductById, deleteProduct, updateProduct, 
+router.route('/products/:id').get(getProduct).patch(updateProduct).delete(removeProduct);
+
+export default router;
+
