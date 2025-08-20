ee4d23f87ec82171f0d40b76e0310d43520c0a27
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-20 07:07:51 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router-dom";
 import RootLayOut from "./components/RootLayOut";
-import ArticleList from "./features/articles/ArticleList.jsx";
-import AddArticleForm from "./features/articles/AddArticleForm.jsx";
 
 
 
@@ -16,22 +14,15 @@ export default function App() {
       children: [
         {
           index: true,
-          element: <ArticleList />
+
         },
-        {
-          path: 'add-article',
-          element: <AddArticleForm />
-        }
+
 
 
       ]
     },
 
 
-    // {
-    //   path: '*',
-    //   element: <NotFound />
-    // }
   ]);
 
   return <RouterProvider router={router} />
