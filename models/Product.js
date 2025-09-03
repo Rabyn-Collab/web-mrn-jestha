21e0b1e3cc8ac0cff837173d065683f13d61fab8
Rabyn-Collab <babynshrestha64@gmail.com>
2025-09-03 08:01:45 +0545

    changes

--- models/Product.js
+++ models/Product.js
@@ -61,7 +61,24 @@ const productSchema = new mongoose.Schema({
   rating: {
     type: Number,
     default: 0
-  }
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      rating: {
+        type: Number,
+        required: true
+      },
+      comment: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 }, { timestamps: true });
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { loginSchema, registerSchema, validatorJoi } from '../utils/validator.js';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { getUser, loginUser, registerUser, updateUser } from '../controllers/userController.js';
 import { methodNotAllowed } from '../utils/methodNotAllowed.js';
+import { checkUser } from '../middlewares/checkAuth.js';
 
 
 
 const router = express.Router();
+
+
+router.route("/users").get(checkUser, getUser).all(methodNotAllowed);
+router
+  .route("/users/update")
+  .patch(checkUser, updateUser)
+  .all(methodNotAllowed);
+
 
 router.route('/users/login').post(validatorJoi.body(loginSchema), loginUser).all(methodNotAllowed);
 router.route('/users/register').post(validatorJoi.body(registerSchema), registerUser).all(methodNotAllowed);
